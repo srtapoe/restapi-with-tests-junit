d2ae55f8395a34179c81bce07b0d7b9387d70866
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-03-02 20:36:14 -0300

    Initial commit

---
+++ src/main/java/com/br/studies/restapiwithtestsjunit/RestapiWithTestsJunitApplication.java
@@ -0,0 +1,13 @@
+package com.br.studies.restapiwithtestsjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiWithTestsJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiWithTestsJunitApplication.class, args);
+    }
+
+}
